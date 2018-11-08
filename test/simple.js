4225f6ea18a1e00312a55992274b97348c9a75fb
Jan Bölsche <regular.gonzales@googlemail.com>
2018-11-08 15:42:35 +0100

    Add test for sync=false

--- test/simple.js
+++ test/simple.js
@@ -14,11 +14,40 @@ function append(db, msgs, cb) {
       setTimeout( ()=>cb(null, x), 100)
     }),
     pull.collect( (err, seqs)=>{
-      if (err) throw err
+      if (err) {
+        console.error('append failed')
+        throw err
+      }
       cb(seqs)
     })
   )
 }
+
+test('sync: false', (t, db) => {
+  const keyA = rndKey()
+  const keyB = rndKey()
+  const a = msg(keyA)
+  const b = msg(keyB)
+
+  a.value.content.type = 'cat'
+  b.value.content.type = 'cat'
+
+  append(db, [a, b], seqs => {
+
+    const cats = MutantArray()
+
+    pull(
+      db.revisions.messagesByType('cat'),
+      collectMutations(cats, err  => {
+        if (err) throw err
+        t.deepEqual(cats.getLength(), 2)
+        t.ok(cats().map(kv=>kv.key).includes(a.key))
+        t.ok(cats().map(kv=>kv.key).includes(b.key))
+        t.end()
+      })
+    )
+  })
+})
 
 test('morphing', (t, db) => {
   const keyA = rndKey()
